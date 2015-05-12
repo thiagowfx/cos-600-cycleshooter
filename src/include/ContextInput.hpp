efd28923d8ad7d6979fa61dd4542c421f15c70e4
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2015-05-12 00:08:50 -0300

    Creating a ContextInput class to manage UserInput

---
+++ src/include/ContextInput.hpp
@@ -0,0 +1,25 @@
+#ifndef _CONTEXTINPUT_HPP_
+#define _CONTEXTINPUT_HPP_
+
+namespace Cycleshooter {
+class ContextInput{
+	public:
+		enum InputMode {
+			CONTEXT_LAB
+			CONTEXT_DEBUG
+			CONTEXT_NONE
+		};
+
+	private:
+		InputMode inputMode;
+		
+	public:
+		ContextInput();
+		virtual ~ContextInput();
+
+		void setupDebugInput();
+		void setupLabInput();
+};	
+}
+
+#endif
