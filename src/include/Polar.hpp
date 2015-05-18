7520c6843e96df3264be96d2ae961653f23920c5
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2015-05-17 22:58:15 -0300

    Modeling the Polar class.

--- src/include/Polar.hpp
+++ src/include/Polar.hpp
@@ -3,14 +3,50 @@
 
 #include "AbstractPolar.hpp"
 
+// C++ headers
+#include <iostream>
+#include <cstring>
+
+// C headers to handle with system and serial Ports
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <paths.h>
+#include <termios.h>
+#include <sysexits.h>
+#include <sys/param.h>
+#include <sys/select.h>
+
+#define MAX_STRING_RESPONSE 140
+
 namespace Cycleshooter {
 class Polar : public AbstractPolar {
+
+private:
+    const char *serialPort;
+    int serialDescriptor;
+    struct termios gOriginalTTYAttrs;
 
 public:
     Polar();
     virtual ~Polar();
     unsigned short readInstantaneousHeartRate();
     unsigned short readMeanHeartRate();
+
+    void setupSerialPort(const char *deviceFilePath);
+
+private:
+    // Functions to deal with the serial Port in an Unix env.
+    int openSerialPort(const char *deviceFilePath);
+    void closeSerialPort();
+
+    // Function to send a command to get the number of heart rate
+    // values specified in NumEntries
+    bool SendGetHeartRate(int fd, int NumEntries);
+
+    // Function to read a response string back from the HRMI
+    int GetResponseString(int fd, char* ResponseString);
 };
 
 }
--- src/src/Polar.cpp
+++ src/src/Polar.cpp
@@ -3,9 +3,11 @@
 namespace Cycleshooter {
 
 Polar::Polar() {
+
 }
 
 Polar::~Polar() {
+
 }
 
 unsigned short Polar::readInstantaneousHeartRate(){
@@ -16,5 +18,70 @@ unsigned short Polar::readMeanHeartRate(){
 
 }
 
+int Polar::openSerialPort(const char *deviceFilePath){
+    // variables
+    int fd = -1;              // file descriptor for serial port
+    struct termios options;   // serial port configuration options
+
+    // Open the serial port
+    if ((fd = open(deviceFilePath, O_RDWR | O_NOCTTY )) == -1) {
+        std::cout << "Error opening serial port " << deviceFilePath << " - " <<
+                strerror(errno) << "(" << errno << ")" << std::endl;
+        return(-1);
+    }
+
+    // Prevent other processes from opening the serial port
+    if (ioctl(fd, TIOCEXCL) == -1) {
+        std::cout << "Error setting TIOCEXCL on " << deviceFilePath << " - " <<
+                strerror(errno) << "(" << errno << ")" << std::endl;
+        return(-1);
+    }
+
+    // Get the serial port current options and save them to restore on exit
+    if (tcgetattr(fd, &gOriginalTTYAttrs) == -1) {
+        std::cout << "Error getting tty attributes" << deviceFilePath << " - " <<
+                strerror(errno) << "(" << errno << ")" << std::endl;
+        return(-1);
+    }
+
+    // Configure the serial port
+    options = gOriginalTTYAttrs;
+    //   Set raw input (non-canonical) mode, with reads blocking until either a
+    //   single character has been received or a one second timeout expires
+    cfmakeraw(&options);
+    options.c_cc[VMIN] = 1;
+    options.c_cc[VTIME] = 10;
+    //    Set the baud rate and word length
+    cfsetspeed(&options, B9600);
+    options.c_cflag |= (CS8);
+
+    //    Cause the new options to take effect immediately
+    if (tcsetattr(fd, TCSANOW, &options) == -1) {
+        std::cout << "Error setting tty attributes!" << std::endl;
+        return(-1);
+    }
+    // Saving the state of the file descriptor (To close the serial in the future)
+    serialDescriptor = fd;
+    return(fd);
 }
+
+void Polar::closeSerialPort(){
+
+}
+
+bool Polar::SendGetHeartRate(int fd, int NumEntries){
+
+}
+
+int Polar::GetResponseString(int fd, char* ResponseString){
+
+}
+
+void Polar::setupSerialPort(const char *deviceFilePath){
+    serialPort = deviceFilePath;
+}
+
+}
+
+
 
