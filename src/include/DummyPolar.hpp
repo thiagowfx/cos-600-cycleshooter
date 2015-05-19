2551502caf0ddc0a5b2ec8d0ae3098e18f7eafad
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2015-05-19 17:13:50 -0300

    Implementing the couple methods of DummyPolar. Need to be tested.

--- src/include/DummyPolar.hpp
+++ src/include/DummyPolar.hpp
@@ -3,6 +3,10 @@
 
 #include "AbstractPolar.hpp"
 
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
 namespace Cycleshooter {
 class DummyPolar : public AbstractPolar {
 public:
@@ -11,17 +15,22 @@ public:
 private:
     short minHeartPeak;
     short maxHeartPeak;
+    std::vector<int> recordHeartRate;
 
 public:
     DummyPolar();
     virtual ~DummyPolar();
 
+    // Setup the min and max values to rand.
+    // Tip : Use short intervals (e.g, 80 - 90)
     void setupHeartPeaks(short min, short max);
 
+    // Get a single value to represent the Heart Rate and save it
     short readInstantaneousHeartRate();
-    short readMeanHeartRate();
 
-private:
+    // Get the mean of "itrMeanHeartRate" values.
+    //Useful to get the base Heart Rate in the beginning of the game
+    short readMeanHeartRate();
 
 };
 
--- src/src/DummyPolar.cpp
+++ src/src/DummyPolar.cpp
@@ -3,17 +3,59 @@
 namespace Cycleshooter {
 
 DummyPolar::DummyPolar() {
+    // Initialize the size of the vector to get the mean value
+    itrMeanHeartRate = 200;
+    // Clear the vector (Guarantee the vector initialize empty)
+    recordHeartRate.clear();
 }
 
 DummyPolar::~DummyPolar() {
+    // Clear the vector
+    recordHeartRate.clear();
+}
+
+void DummyPolar::setupHeartPeaks(short min, short max){
+    minHeartPeak = min;
+    maxHeartPeak = max;
 }
 
 short DummyPolar::readInstantaneousHeartRate(){
 
+    short heartRate;
+
+    // Initialize random seed
+    srand (time(NULL));
+
+    // Generate a random number
+    heartRate = rand() % (maxHeartPeak-minHeartPeak) + minHeartPeak;
+
+    // Save the value
+    if(recordHeartRate.size() < itrMeanHeartRate){
+        recordHeartRate.push_back(heartRate);
+    } else {
+        recordHeartRate.erase(recordHeartRate.begin() + 1);
+        recordHeartRate.push_back(heartRate);
+    }
+
+    return heartRate;
 }
 
 short DummyPolar::readMeanHeartRate(){
+    int sum = 0;
 
+    if (recordHeartRate.empty()){
+        // If the vector is empty, return the instantaneous value
+        return readInstantaneousHeartRate();
+
+    } else {
+        // Calculate the sum of all elements in the vector
+        for(std::vector<int>::iterator it=recordHeartRate.begin();
+            it!=recordHeartRate.end();++it)
+            sum += *it;
+
+        // return the mean of the vector
+        return (sum/recordHeartRate.size());
+
+    }
 }
-
 }
