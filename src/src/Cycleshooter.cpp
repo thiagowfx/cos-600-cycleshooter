2baa411192e95aba544f4bde31dcaaa9be1c62e4
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-06-01 21:25:25 -0300

    add usage

--- src/src/Cycleshooter.cpp
+++ src/src/Cycleshooter.cpp
@@ -1,22 +1,20 @@
+#include <cstring>
 #include "Controller.hpp"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// TODO: use argc and argv
 int main(int argc, char *argv[]) {
     try {
-        Cycleshooter::Controller* app  = new Cycleshooter::Controller(argc, argv);
-        delete app;
+        if (argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[2], "--help"))) {
+            std::cout << argv[0] << " [<fullscreen> [<width> <height>]]" << std::endl;
+            std::cout << "Examples:" << std::endl;
+            std::cout << "\t" << argv[0] << " 0 1366 768" << std::endl;
+            std::cout << "\t" << argv[0] << " 1 800 600" << std::endl;
+        }
+        else {
+            Cycleshooter::Controller(argc, argv);
+        }
     } catch(Ogre::Exception& e)  {
-        std::cerr << "An exception has occurred: " <<
-                     e.getFullDescription().c_str() << std::endl;
+        std::cerr << "An exception has occurred: " << e.getFullDescription().c_str() << std::endl;
     }
 
     return 0;
 }
-
-#ifdef __cplusplus
-}
-#endif
