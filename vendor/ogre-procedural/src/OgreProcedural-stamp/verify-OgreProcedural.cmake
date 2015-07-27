431f02319e739bf2ae2039054c2fedac02110998
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-07-26 22:47:05 -0300

    remove ogre-procedural mess introduced in a random commit by Matheus

--- vendor/ogre-procedural/src/OgreProcedural-stamp/verify-OgreProcedural.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/home/matheus/cycleshooter/vendor/ogre-procedural/src/ca6eb3363a53.zip")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
