f97a9e8e0aeea61373bf2c57df2c70380b6f7f26
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2015-08-01 16:26:44 -0300

    Fixing color values of the pixels

--- src/src/CollisionHandler.cpp
+++ src/src/CollisionHandler.cpp
@@ -55,14 +55,10 @@ void CollisionHandler::loadTensor(){
             // Outside Circuit Materials
             else if(collisionPixel == START_COLOR)
                 collisionMatrix[row][col] = START_PIXEL;
-            else if (collisionPixel == PATH_COLOR){
-                //std::cout <<-row*(25000.0/2048) + 12500 << " 0.0 " << -col*(25000.0/2048) + 12500 << std::endl;
+            else if (collisionPixel == PATH_COLOR)
                 collisionMatrix[row][col] = PATH_PIXEL;
-            }
             else if(collisionPixel == GRASS_COLOR)
                 collisionMatrix[row][col] = GRASS_PIXEL;
-            else if(collisionPixel == WATER_COLOR)
-                collisionMatrix[row][col] = WATER_PIXEL;
         }
     }
 }
